f4470619195b1d0b9d4642788db5c136e3490a0f
minhan <alsgks1218@gmail.com>
2020-02-18 00:05:27 +0900

    login page

--- blog-frontend/src/index.js
+++ blog-frontend/src/index.js
@@ -3,10 +3,18 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 import { BrowserRouter as Router } from "react-router-dom";
+import { Provider } from "react-redux";
+import { createStore } from "redux";
+import { composeWithDevTools } from "redux-devtools-extension";
+import rootReducer from "./modules";
+
+const store = createStore(rootReducer, composeWithDevTools());
 
 ReactDOM.render(
-  <Router>
-    <App />
-  </Router>,
+  <Provider store={store}>
+    <Router>
+      <App />
+    </Router>
+  </Provider>,
   document.getElementById("root")
 );
