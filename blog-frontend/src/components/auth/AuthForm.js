f782f6a0dce75571005de23e89c09f2a6cb00481
unknown <alsgks1218@naver.com>
2020-02-18 17:59:32 +0900

    before add api

--- blog-frontend/src/components/auth/AuthForm.js
+++ blog-frontend/src/components/auth/AuthForm.js
@@ -42,26 +42,56 @@ const Footer = styled.div`
   }
 `;
 
-const AuthForm = () => {
+const ButtonWithMarginTop = styled(Button)`
+  margin-top: 1rem;
+`;
+
+const textMap = {
+  login: "로그인",
+  register: "회원가입"
+};
+
+const AuthForm = ({ type, form, onChange, onSubmit }) => {
+  const text = textMap[type];
   return (
     <AuthFormBlock>
-      <h3>로그인</h3>
-      <form>
+      <h3>{text}</h3>
+      <form onSubmit={onSubmit}>
         <StyledInput
           autoComplete="username"
           name="username"
           placeholder="아이디"
+          value={form.username}
+          onChange={onChange}
         />
         <StyledInput
           autoComplete="new-password"
           name="password"
           placeholder="비밀번호"
           type="password"
+          value={form.password}
+          onChange={onChange}
         />
-        <Button>로그인</Button>
+        {type === "register" && (
+          <StyledInput
+            autoComplete="new-password"
+            name="passwordConfirm"
+            placeholder="비밀번호 확인"
+            type="password"
+            value={form.passwordConfirm}
+            onChange={onChange}
+          />
+        )}
+        <ButtonWithMarginTop cyan fullWidth>
+          {text}
+        </ButtonWithMarginTop>
       </form>
       <Footer>
-        <Link to="/register">회원가입</Link>
+        {type === "login" ? (
+          <Link to="/register">회원가입</Link>
+        ) : (
+          <Link to="/login">로그인</Link>
+        )}
       </Footer>
     </AuthFormBlock>
   );
