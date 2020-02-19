2230466774b887ac9416f6fafa65e4f01d652787
minhan <alsgks1218@gmail.com>
2020-02-20 01:00:34 +0900

    before chapter 25

--- blog-frontend/src/components/auth/AuthForm.js
+++ blog-frontend/src/components/auth/AuthForm.js
@@ -51,7 +51,14 @@ const textMap = {
   register: "회원가입"
 };
 
-const AuthForm = ({ type, form, onChange, onSubmit }) => {
+const ErrorMessage = styled.div`
+  color: red;
+  text-align: center;
+  font-size: 0.875rem;
+  margin-top: 1rem;
+`;
+
+const AuthForm = ({ type, form, onChange, onSubmit, error }) => {
   const text = textMap[type];
   return (
     <AuthFormBlock>
@@ -82,6 +89,7 @@ const AuthForm = ({ type, form, onChange, onSubmit }) => {
             onChange={onChange}
           />
         )}
+        {error && <ErrorMessage>{error}</ErrorMessage>}
         <ButtonWithMarginTop cyan fullWidth>
           {text}
         </ButtonWithMarginTop>
--- blog-frontend/src/containers/auth/RegisterForm.js
+++ blog-frontend/src/containers/auth/RegisterForm.js
@@ -1,14 +1,18 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { changeField, initializeForm, register } from "../../modules/auth";
 import AuthForm from "../../components/auth/AuthForm";
+import { check } from "../../modules/user";
+import { withRouter } from "react-router-dom";
 
-const RegisterForm = () => {
+const RegisterForm = ({ history }) => {
+  const [error, setError] = useState(null);
   const dispatch = useDispatch();
-  const { form, auth, authError } = useSelector(({ auth }) => ({
+  const { form, auth, authError, user } = useSelector(({ auth, user }) => ({
     form: auth.register,
     auth: auth.auth,
-    authError: auth.authError
+    authError: auth.authError,
+    user: user.user
   }));
 
   const onChange = e => {
@@ -19,7 +23,14 @@ const RegisterForm = () => {
   const onSubmit = e => {
     e.preventDefault();
     const { username, password, passwordConfirm } = form;
+    if ([username, password, passwordConfirm].includes("")) {
+      setError("빈 칸을 모두 입력하세요.");
+      return;
+    }
     if (password !== passwordConfirm) {
+      setError("비밀번호가 일치하지 않습니다.");
+      changeField({ form: "register", key: "password", value: "" });
+      changeField({ form: "register", key: "passwordConfirm", value: "" });
       return;
     }
     dispatch(register({ username, password }));
@@ -31,6 +42,11 @@ const RegisterForm = () => {
 
   useEffect(() => {
     if (authError) {
+      if (authError.response.status === 409) {
+        setError("이미 존재하는 계정명입니다.");
+        return;
+      }
+      setError("회원가입 실패");
       console.log("오류 발생");
       console.log(authError);
       return;
@@ -38,8 +54,22 @@ const RegisterForm = () => {
     if (auth) {
       console.log("회원가입 성공");
       console.log(auth);
+      dispatch(check());
     }
-  });
+  }, [auth, authError, dispatch]);
+
+  useEffect(() => {
+    if (user) {
+      console.log("check API 성공");
+      console.log(user);
+      history.push("/");
+      try {
+        localStorage.setItem("user", JSON.stringify(user));
+      } catch (e) {
+        console.log("localStorage is not working");
+      }
+    }
+  }, [history, user]);
 
   return (
     <AuthForm
@@ -47,8 +77,9 @@ const RegisterForm = () => {
       form={form}
       onChange={onChange}
       onSubmit={onSubmit}
+      error={error}
     />
   );
 };
 
-export default RegisterForm;
+export default withRouter(RegisterForm);
