75301b2feda38b8a0f67a41ef3945ede9a9a4b53
unknown <alsgks1218@naver.com>
2020-02-20 17:53:06 +0900

    before Editor component revision

--- blog-frontend/src/pages/WritePage.js
+++ blog-frontend/src/pages/WritePage.js
@@ -1,7 +1,17 @@
 import React from "react";
+import TagBox from "../components/write/TagBox";
+import Responsive from "../components/common/Responsive";
+import WriteActionButtons from "../components/write/WriteActionButton";
+import EditorContainer from "../containers/write/EditorContainer";
 
 const WritePage = () => {
-  return <div>글쓰기</div>;
+  return (
+    <Responsive>
+      <EditorContainer />
+      <TagBox />
+      <WriteActionButtons />
+    </Responsive>
+  );
 };
 
 export default WritePage;
